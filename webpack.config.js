b2deae67fee8262d3cd4a17252874f17624f4e8f
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-07-14 10:35:35 +0000

    moved to deveployment mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,13 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    mode: "development",
+    mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
     },
-    devtool: "eval-source-map",
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
